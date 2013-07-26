1fb436491a22e71832423871c542533be23ac070
Otacilio <otacilioferreiro@gmail.com>
2013-07-26 20:00:53 -0300

    Atualização 26/07 - Inclusão das Activity
    Inclusão das Activity que faltavam, juntamente de seus layouts.

--- E-Point/gen/br/androidapp/unialliance/e_point/R.java
+++ E-Point/gen/br/androidapp/unialliance/e_point/R.java
@@ -32,8 +32,13 @@ public final class R {
         public static final int textViewPass=0x7f080002;
     }
     public static final class layout {
-        public static final int activity_login=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int activity_checkedpoint=0x7f030000;
+        public static final int activity_historicview=0x7f030001;
+        public static final int activity_home=0x7f030002;
+        public static final int activity_login=0x7f030003;
+        public static final int activity_main=0x7f030004;
+        public static final int activity_msgbox=0x7f030005;
+        public static final int activity_timetable=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f070000;
